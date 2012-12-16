ebc92b8fb042f399da6294ec810607dcf68ca95f
davidparracatalan <davidparracatalan@gmail.com>
2012-12-17 00:27:50 +0100

    some view refactoring...

--- spec/requests/teammate_assignments_page_spec.rb
+++ spec/requests/teammate_assignments_page_spec.rb
@@ -5,32 +5,43 @@ describe "Teammate's assignments page" do
   subject {page}
 
   let!(:mate) {FactoryGirl.create(:teammate)}
-  let!(:subteam_1) {FactoryGirl.create(:subteam)}
-  let!(:subteam_2) {FactoryGirl.create(:subteam)}
 
-  describe "Teammate assignment page without any team assigned" do
+  describe "without available assignments" do
     before do
-      visit team_assignments_teammate_path(mate)
+        visit team_assignments_teammate_path(mate)
     end
-      it {should have_selector('h1', text:"#{mate.name} is not assigned to any subteam yet")}
-      it {should have_selector('span', text:"#{subteam_1.name}")}
-      it {should have_selector('span', text:"#{subteam_2.name}")}
-      it {should have_css("#assignment_button")}
+    it {should have_selector('h1', text:"No subteams available for #{mate.name}")}
   end
 
-  describe "Teammate assignment page with assignments" do
 
-    before do
-      mate.assign_to! subteam_1
-      visit team_assignments_teammate_path(mate)
+  describe "with available assignments" do
+    let!(:subteam_1) {FactoryGirl.create(:subteam)}
+    let!(:subteam_2) {FactoryGirl.create(:subteam)}
+
+    describe "but without any team assigned" do
+      before do
+        visit team_assignments_teammate_path(mate)
+      end
+        it {should have_selector('h1', text:"#{mate.name} is not assigned to any subteam yet")}
+        it {should have_selector('span', text:"#{subteam_1.name}")}
+        it {should have_selector('span', text:"#{subteam_2.name}")}
+        it {should have_css("#assignment_button")}
     end
 
-    it {should have_selector('h1', text:"#{mate.name}'s assignments")}
-    it {should have_selector('h2', text:"#{mate.name} is assigned to the following subteams")}
-    it {find("#assigned_teams_list").should have_selector('li', text:"#{subteam_1.name}")}
-    it {find("#assigned_teams_list").should_not have_selector('li', text:"#{subteam_2.name}")}
-    it {find("#available_teams").should have_selector('span', text:"#{subteam_2.name}")}
-    it {find("#available_teams").should_not have_selector('span', text:"#{subteam_1.name}")}
+    describe "and assignments for the mate" do
+
+      before do
+        mate.assign_to! subteam_1
+        visit team_assignments_teammate_path(mate)
+      end
+
+      it {should have_selector('h1', text:"#{mate.name}'s assignments")}
+      it {should have_selector('h2', text:"#{mate.name} is assigned to the following subteams")}
+      it {find("#assigned_teams_list").should have_selector('span', text:"#{subteam_1.name}")}
+      it {find("#assigned_teams_list").should_not have_selector('span', text:"#{subteam_2.name}")}
+      it {find("#available_teams").should have_selector('span', text:"#{subteam_2.name}")}
+      it {find("#available_teams").should_not have_selector('span', text:"#{subteam_1.name}")}
+    end
   end
 
   describe "Assignning teammate to a  team" do
