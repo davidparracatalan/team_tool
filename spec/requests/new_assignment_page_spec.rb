620dd1fa3f3dcdb3c4b2afb094ee0cd045c4972d
davidparracatalan <davidparracatalan@gmail.com>
2012-12-26 00:00:05 +0100

    a little test on assignments

--- spec/requests/new_assignment_page_spec.rb
+++ spec/requests/new_assignment_page_spec.rb
@@ -8,18 +8,20 @@ describe "New assignment page" do
   let!(:mate){FactoryGirl.create(:teammate)}
   let!(:subteam){FactoryGirl.create(:subteam)}
 
-  before do
-    #Since capybara's visit methos doesn't take more than one param I don't
-    #know how to visit directly the teammate-subteam assignment url. Using a
-    #quite ugly workaround
-    visit subteam_assignments_teammate_path(mate)
-    click_button "Assign"
+  before do 
+    visit ("/teammates/#{mate.id}/assignments/new?subteam_id=#{subteam.id}")
   end
 
   describe "Validating elements in the page" do
     it {should have_selector('h2'), text: "You're assigning #{mate.name} to #{subteam.name}"}
     it {should have_selector('label'), text: "Start date"}
     it {should have_selector('label'), text: "Foreseen end date"}
+    it {should have_button('Confirm assignment')}
+  end
+
+  describe "Confirming the assignment" do
+    before {click_button('Confirm assignment')}
+    it {current_path.should == subteam_assignments_teammate_path(mate)}
   end
 
 end
